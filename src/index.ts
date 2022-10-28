a18e4636cff8ef598daf921971c08c84f23548f2
Nelson Earle <nelson.earle137@gmail.com>
2022-10-28 15:39:06 -0500

    FIXUP: add demo express

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 // Get path of current file, directory, and the parent directory
-// const __filename = url.fileURLToPath(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const __parent = path.dirname(__dirname);
