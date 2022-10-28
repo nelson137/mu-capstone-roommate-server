287533a9f3b30f37b0f24b89ca1526083c2da0ac
Nelson Earle <nelson.earle137@gmail.com>
2022-10-27 20:21:18 -0500

    add db api demo

---
+++ src/db.ts
@@ -0,0 +1,50 @@
+import { connect, model, Schema } from 'mongoose';
+
+// TODO: Would the [connect-mongo package](https://www.npmjs.com/package/connect-mongo) be useful?
+
+// Full schema for model definition
+const userSchema = new Schema({
+    name: String,
+    gender: String,
+    loc: {
+        city: String,
+        state: String,
+    },
+    status: String,
+    smoking: Boolean,
+    pets: Boolean,
+    sameSex: Boolean,
+    age: {
+        min: Boolean,
+        max: Boolean,
+    },
+    rent: {
+        min: Number,
+        max: Number,
+    },
+    noise: String,
+    guests: String,
+    sleep: String,
+    commonSpaces: String,
+    clean: String,
+    tags: {
+        videoGames: Boolean,
+        writing: Boolean,
+        photography: Boolean,
+        plants: Boolean,
+    },
+    extraBoxes: {
+        religion: String,
+        transportation: String,
+    },
+    bio: String,
+    DoB: Date,
+    contactEmail: String,
+});
+const User = model('user', userSchema);
+
+export const setupDatabase = () => connect('mongodb://localhost/bunkiez');
+
+// Projection defines which fields from the doc the query will return
+const userProjection = { name: 1 };
+export const getUsers = () => User.find({}, userProjection);
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,23 @@ import http from 'http';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { setupChat } from './chat';
+import { getUsers, setupDatabase } from './db';
 
 // Get path of current file, directory, and the parent directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const __parent = path.dirname(__dirname);
 
+await setupDatabase();
+
 const app = express();
 
 app.get('/', (_req: Request, res: Response) => {
     res.sendFile(`${__parent}/index.html`);
+});
+
+app.get('/users', async (_req: Request, res: Response) => {
+    res.send(await getUsers());
 });
 
 const server = http.createServer(app);
