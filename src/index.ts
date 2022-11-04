5f7e7aaa196c6903fa198d364d8ef031d3e31ae9
Nelson Earle <nelson.earle137@gmail.com>
2022-11-03 20:14:15 -0500

    fix CORS & JSON header issues

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
@@ -26,7 +27,9 @@ await setupDatabase();
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+
+app.use(express.json({ type: '*/*' }));
 
 app.use('/', appRouter);
 
