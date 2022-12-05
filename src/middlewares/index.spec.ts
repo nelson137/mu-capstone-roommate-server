2fed62b8e7fe8764730ad41f5e6c2239810848c1
Malek N <Malek@Necibi.us>
2022-12-04 19:34:58 -0600

    changes to accommodate remote server

--- src/middlewares/index.spec.ts
+++ src/middlewares/index.spec.ts
@@ -4,59 +4,9 @@ import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { buildErrors, mockResponse } from 'src/test-utils';
 import supertest from 'supertest';
-import { authorize, guardValidation } from '.';
+import { guardValidation } from '.';
 
 const chance = new Chance();
-
-describe('authorize', () => {
-    let request: Partial<Request>;
-    let response: Response;
-    const next: NextFunction = jest.fn();
-
-    beforeEach(() => {
-        request = {
-            headers: {},
-        };
-        response = mockResponse();
-    });
-
-    it('should return 401 when there is no authorization header', () => {
-        authorize(request as Request, response, next);
-
-        expect(response.status).toHaveBeenCalledWith(401);
-        expect(response.json).toHaveBeenCalledWith(buildErrors('no token'));
-        expect(next).not.toHaveBeenCalled();
-    });
-
-    it('should return 401 when the authorization header is invalid', () => {
-        request.headers!.authorization = chance.string();
-
-        authorize(request as Request, response, next);
-
-        expect(response.status).toHaveBeenCalledWith(401);
-        expect(response.json).toHaveBeenCalledWith(buildErrors('invalid token'));
-        expect(next).not.toHaveBeenCalled();
-    });
-
-    it('should return 401 when the auth token is invalid', () => {
-        request.headers!.authorization = 'Bearer ' + chance.string();
-
-        authorize(request as Request, response, next);
-
-        expect(response.status).toHaveBeenCalledWith(401);
-        expect(response.json).toHaveBeenCalledWith(buildErrors('JsonWebTokenError: jwt malformed'));
-        expect(next).not.toHaveBeenCalled();
-    });
-
-    it('should not return anything and call the next middleware when the auth token is valid', () => {
-        const token = jwt.sign({}, process.env.JWT_SECRET!);
-        request.headers!.authorization = 'Bearer ' + token;
-
-        authorize(request as Request, response, next);
-
-        expect(next).toHaveBeenCalled();
-    });
-});
 
 describe('guardValidation', () => {
     let request: Partial<Request>;
